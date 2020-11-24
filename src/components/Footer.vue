be61574692554f7a3551e26feebcf6caf8ea0da7
Quoc Tan <tqtwork@outlook.com>
2020-11-24 22:57:38 +0700

    header and footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,195 +1,173 @@
 <template>
-  <section class="footer">
-    <div class="container">
-      <footer class="footer__main">
-        <div class="ptm">
-          <a title="Home" href="/"
-            ><img
-              class=" ls-is-cached lazyloaded"
-              data-src="/img/logo_new.svg"
-              alt="canned responses typedesk logo"
-              width="150px"
-              src="/img/logo_new.svg"
-          /></a>
-          <p class="txt--muted">
-            Ditch repetitive typing with powerful canned responses for you and
-            your team.
+  <div class="container">
+    <div class="footer">
+      <!-- Main footer -->
+      <div class="grid-container">
+        <div class="grid-item">
+          <router-link tag="a" class="logo" to="/"
+            ><div class="logo-wrapper">
+              <img src="/src/assets/logo.svg" alt="logo" /></div
+          ></router-link>
+          <p class="text-muted">Ditch repetitive typing with powerful canned responses for you and your team.</p>
+          <p class="text-mail">
+            <img class="icon-margin" src="/src/assets/mail_icon.png" alt="mail-icon">
+            support@typedesk.com
           </p>
-          <p class="flex txt--muted">
-            <img
-              class="mrxs"
-              src="/img/mail_icon.png"
-              width="12"
-              alt=""
-            />support@typedesk.com
-          </p>
-          <div class="mtm flex">
-            <a
-              class="mrxs"
-              href="https://www.facebook.com/typedesk/"
-              title="See our Facebook page"
-              ><img src="/img/facebook.svg" width="18" alt=""
-            /></a>
-            <a
-              class="mrxs"
-              href="https://www.linkedin.com/company/typedesk/"
-              title="See our Linkedin page"
-              ><img src="/img/logo_linkedin.png" width="18" alt=""
-            /></a>
-            <a
-              class="mrxs"
-              href="https://www.producthunt.com/posts/typedesk"
-              title="See our Product Hunt profile"
-              ><img src="/img/producthunt.svg" width="18" alt=""
-            /></a>
-            <a
-              class="mrxs"
-              href="https://www.youtube.com/channel/UCOdgGbcZMFLbyFB6H0QCUOA"
-              title="See our YouTube page"
-              ><img src="/img/logo_youtube.png" width="18" alt=""
-            /></a>
+          <div class="icon-link">
+            <a href="#" title="See our Facebook page">
+              <img class="icon-margin" src="/src/assets/facebook.svg" alt="facebook-icon">
+            </a>
+            <a href="#" title="See our LinkedIn page">
+              <img class="icon-margin" src="/src/assets/logo_linkedin.png" alt="linkedin-icon">
+            </a>
+            <a href="#" title="See our Product Hunt page">
+              <img class="icon-margin" src="/src/assets/producthunt.svg" alt="producthunt-icon">
+            </a>
+            <a href="#" title="See our Youtube page">
+              <img class="icon-margin" src="/src/assets/logo_youtube.png" alt="youtube-icon">
+            </a>
           </div>
         </div>
-        <div>
-          <h4>
-            Download our apps
-          </h4>
-          <a title="Download app for Windows" href="/download/windows/"
-            >Windows</a
-          >
-          <a title="Download app for Max" href="/download/mac/">Mac</a>
-          <a title="Download app for Chrome" href="/download/browser/"
-            >Chrome extension</a
-          >
-          <a title="Download add-on for Firefox" href="/download/browser/"
-            >Firefox extension</a
-          >
-          <a title="Open web app" href="/app/">Web app</a>
+        <div class="grid-item">
+          <a><h4>Download our apps</h4></a>
+            <a href="#">Windows</a>
+            <a href="#">Mac</a>
+            <a href="#">Chrome extension</a>
+            <a href="#">Firefox extension</a>
+            <a href="#">Web app</a>
         </div>
-
-        <div>
+        <div class="grid-item">
           <h4>About</h4>
-          <a title="Use Cases" href="/use-cases/">Use Cases</a>
-          <a title="Features" href="/features/">Features</a>
-          <a title="Pricing" href="/pricing/">Pricing</a>
-          <a title="Help Center" href="/help/">Help</a>
-          <a title="Contact us" href="/contact/">Contact</a>
-          <a title="Read our blog" href="/blog/">Blog</a>
+            <a href="#">Use Cases</a>
+            <a href="#">Features</a>
+            <a href="#">Pricing</a>
+            <a href="#">Help</a>
+            <a href="#">Contact</a>
+            <a href="#">Blog</a>
         </div>
-        <div class="footer__main__resources">
+        <div class="grid-item">
           <h4>How we compare</h4>
-          <div>
-            <a
-              href="/canned-responses-for-gmail/"
-              title="Canned Responses Gmail"
-              >Canned responses for Gmail</a
-            >
-            <a
-              href="/canned-responses-for-zendesk/"
-              title="Canned Responses Zendesk"
-              >Canned responses for Zendesk</a
-            >
-            <a
-              href="/canned-responses-for-twitter/"
-              title="Canned Responses Twitter"
-              >Canned responses for Twitter</a
-            >
-            <a
-              href="/canned-responses-for-linkedin/"
-              title="Canned Responses Linkedin"
-              >Canned responses for Linkedin</a
-            >
-            <a
-              href="/canned-responses-for-facebook/"
-              title="Canned Responses Facebook"
-              >Canned responses for Facebook</a
-            >
-            <a
-              href="/canned-responses-for-helpcrunch/"
-              title="Canned Responses HelpCrunch"
-              >Canned responses for HelpCrunch</a
-            >
-            <a
-              href="/canned-responses-for-front/"
-              title="Canned Responses Front"
-              >Canned responses for Front</a
-            >
-            <a
-              href="/canned-responses-for-slack/"
-              title="Canned Responses Slack"
-              >Canned responses for Slack</a
-            >
-            <a
-              href="/canned-responses-for-microsoft-teams/"
-              title="Canned Responses Microsoft Teams"
-              >Canned responses for Microsoft Teams</a
-            >
-            <a
-              href="/canned-responses-for-facebook-messenger/"
-              title="Canned Responses Facebook Messenger"
-              >Canned responses for Facebook Messenger</a
-            >
-            <a
-              href="/canned-responses-for-intercom/"
-              title="Canned Responses Intercom"
-              >Canned responses for Intercom</a
-            >
-            <a
-              href="/canned-responses-for-live-chat/"
-              title="Canned Responses Live Chat"
-              >Canned responses for Live Chat</a
-            >
-            <a
-              href="/alternative-to-textexpander/"
-              title="Alternative to TextExpander"
-              >Alternative to TextExpander</a
-            >
-            <a
-              href="/canned-responses-for-hubspot/"
-              title="Canned Responses Hubspot"
-              >Canned responses for Hubspot</a
-            >
-          </div>
-          <a href="#" class="more btn btn--link btn--more"
-            ><img
-              class="mrxs"
-              src="/img/arrow_green_right.png"
-              width="24"
-              alt=""
-            />Show more comparisons</a
-          >
-          <a href="#" class="more btn btn--link btn--less"
-            ><img
-              class="mrxs"
-              src="/img/arrow_green_right.png"
-              width="24"
-              alt=""
-            />Show less comparisons</a
-          >
+          <a href="#">Canned responses for Gmail</a>
+          <a href="#">Canned responses for Zendesk</a>
+          <a href="#">Canned responses for Twitter</a>
+          <a href="#">Canned responses for Linkedin</a>
         </div>
-      </footer>
-      <div class="flex flex--center txt--light txt--center myl">
-        © typedesk 2020
-        <a class="txt--light mxs" title="Terms &amp; Conditions" href="/terms/"
-          >Terms &amp; Conditions</a
-        >
-        <a class="txt--light mxs" title="Privacy policy" href="/privacy/"
-          >Privacy</a
-        >
-
-        <a
-          title="Collaborative roadmap"
-          class="txt--light mxs"
-          href="https://typedesk.hellonext.co/"
-          >Vote for our next features</a
-        >
+      </div>
+      <!-- Last line -->
+      <div class="flex-container">
+        <div class="info">&#169; typedesk 2020</div>
+        <div class="info">
+          <a href="#">Terms and Conditions</a>
+        </div>
+        <div class="info">
+          <a href="#">Privacy</a>
+        </div>
+        <div class="info">
+          <a href="#">Vote for our next features</a>
+        </div>
       </div>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+/* Footer general style */
+.footer {
+  margin-top: 100px;
+}
+
+a {
+  list-style-type: none;
+  color: #99b3bd;
+}
+
+h4 {
+  line-height: 1.05;
+  font-size: 1.1rem;
+}
+
+/* Main footer */
+.grid-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gap: 30px;
+  align-items: center !important;
+  justify-content: center !important;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  margin-top: 40px;
+  align-items: start !important;
+}
+
+.grid-item {
+  display: flex;
+  flex-direction: column;
+}
+
+.grid-item a {
+  color: #1c1d1e;
+}
+
+.grid-item h4 {
+  margin-top: 0;
+}
+
+.logo {
+  width: 100%;
+}
+
+.logo-wrapper {
+  width: 8rem;
+  display: inline-block;
+}
+
+.logo-wrapper img {
+  width: 100%;
+  align-self: center;
+  display: flex;
+}
+
+.text-muted, .text-mail{
+  color: #99b3bd !important;
+  display: flex;
+  align-items: center;
+}
+
+.text-mail > img {
+  height: 10px;
+  background-color:#f5f6fa !important;
+}
+
+.icon-margin {
+  margin-right: 11px;
+}
+
+.icon-link {
+  margin-top: 25px;
+}
+
+.icon-link img {
+  height: 16px;
+}
+
+/* Last line footer info */
+
+.flex-container {
+  display: flex;
+  align-items: center !important;
+  justify-content: center !important;
+  margin-top: 40px;
+  margin-bottom: 40px;
+}
+
+.flex-container .info {
+  color: #99b3bd;
+  margin-left: 16px;
+  margin-right: 16px;
+  text-align: center;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,12 @@
 <template>
     <div class="home">
-        <h1 style="border: 1px solid gray;">Sample text to align item</h1>
+        <h1 style="border: 1px solid gray;">Home destination</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam sunt ut nesciunt in laboriosam mollitia eos earum beatae iusto hic rerum sint, nisi quos error odio fuga. Fugit, atque quam.</p>
     </div>
 </template>
 
 <script>
-export default {
-    name: 'home',
-    data() {
-        return {
-
-        }
-    },
-
-}
+export default {}
 </script>
 
 <style>
